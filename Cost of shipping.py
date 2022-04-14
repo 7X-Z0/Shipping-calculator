102d186cd84caadc501aa6a4639132ab272bd844
7X_Z0 <lorenzoguzman205@gmail.com>
2022-04-14 08:15:23 -0400

    While command doesn't exit code when receiving invalid inputs

--- Cost of shipping.py
+++ Cost of shipping.py
@@ -4,11 +4,16 @@ print('Made by: Lorenzo Guzman')
 
 print('-------------------------------')
 
-weight = int(input('Please input package weight'))
+while True:
+    weight = float(input('Please input package weight: '))
+    if weight < 0:
+        print('invalid input')
+    elif weight > 20:
+        print('Package cannot be shipped')
+    else:
+        break
 
-if weight < 0:
-    print('invalid input')
-elif 0 < weight <= 1:
+if 0 < weight <= 1:
     print('The shipping cost is $3.5')
 elif 1 < weight <= 3:
     print('The shipping cost is $5.5')
@@ -16,6 +21,5 @@ elif 3 < weight <= 10:
     print('The shipping cost is $8.5')
 elif 10 < weight <= 20:
     print('The shipping cost is $10.5')
-else:
-    print('invalid input')
+
 
